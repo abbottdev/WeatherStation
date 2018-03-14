ea5cfee7c1f601edbf3479c139ccef5aaf46f37a
James Abbott <jamesbabbott@gmail.com>
2018-03-14 14:50:00 +0000

    Implemented material design theme, using off-the-shelf frameworks to maximise time spent on code.

--- WeatherStation.Windows/MainWindow.xaml.cs
+++ WeatherStation.Windows/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +23,19 @@ namespace WeatherStation.Windows
     public partial class MainWindow : Window
     {
 
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
 
         public AppViewModel AppViewModel
         {
